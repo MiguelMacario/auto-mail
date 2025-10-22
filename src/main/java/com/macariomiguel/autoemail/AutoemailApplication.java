a73c9e375686c3a7aa7dac875517eaaff590bff0
Miguel Macario <miguel251028@gmail.com>
2025-10-21 21:15:50 -0300

    feat: creation of entities

---
+++ src/main/java/com/macariomiguel/autoemail/AutoemailApplication.java
@@ -0,0 +1,13 @@
+package com.macariomiguel.autoemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoemailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macariomiguel/autoemail/AutoemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macariomiguel.autoemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
